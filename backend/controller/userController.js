a9ac7ba2cd431de4ba8732c4dbeb523045c65147
legionJP <jjpsingh18@gmail.com>
2022-08-02 17:00:54 -0700

    changes in user and tabel interference

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -7,12 +7,12 @@ const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   try {
     if (name === "" || email === "" || password === "") {
-      throw new Error("Fill All details");
+      throw new Error("Please Fill All the details");
     }
 
     const userExist = await User.findOne({ email });
     if (userExist) {
-      throw new Error("User already exist");
+      throw new Error(" This User already exist");
     }
 
     const user = await User.create({ name, email, password, tableName: [] });
@@ -21,7 +21,7 @@ const registerUser = async (req, res) => {
         .status(201)
         .json({ token: generateToken(user._id, user.name, user.email, user.tableName) });
     } else {
-      throw new Error("Invalid User data");
+      throw new Error(" Opps! Invalid User data");
     }
   } catch (error) {
     res.status(400).json({ error: error.toString() });
@@ -34,7 +34,7 @@ const loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     if (email === "" || password === "") {
-      throw new Error("Fill All details");
+      throw new Error(" Please Fill All details");
     }
     const user = await User.findOne({ email });
     if (user && (await user.matchPassword(password))) {
@@ -42,7 +42,7 @@ const loginUser = async (req, res) => {
         .status(201)
         .json({ token: generateToken(user._id, user.name, user.email, user.tableName) });
     } else {
-      throw new Error("incorrect email password");
+      throw new Error("you have entered wrong email password");
     }
   } catch (error) {
     res.status(401).json({ error: error.toString() });
